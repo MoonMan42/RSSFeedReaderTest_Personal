d262b28fbe46836fc84bfdf34d7bc26268b7bc76
MoonMan42 <miller42matt@gmail.com>
2020-02-23 12:54:25 -0500

    Add project files.

---
+++ RSSFeedReaderTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace RSSFeedReaderTest
+{
+    class Program
+    {
+        public static object SyndicationFeed { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string url = "https://news.google.com/rss?hl=en-US&tab=wn&gl=US&ceid=US:en";
+
+            using (XmlReader reader = XmlReader.Create(url))
+            {
+            }
+        }
+    }
+}
---
+++ RSSReaderFrameworkTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RSSReaderFrameworkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://news.google.com/news/rss";
+
+
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            foreach (SyndicationItem item in feed.Items)
+            {
+                Console.WriteLine("Title: {0}", item.Title.Text);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
